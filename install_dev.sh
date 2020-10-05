10a7b309da57184d794b4efba502ba7745c3ab8d
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-05 11:05:22 +0800

    Created: bash scripts to orchestrate running of all svc

--- install_dev.sh
+++ install_dev.sh
@@ -1,6 +1,5 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-# add in services here
-
+# add in services here, change the folder name below to your services, using "user" service as a sample
 cd "$parent_path"/services/v1/user
 pip3 install --no-cache-dir -r $parent_path/services/v1/user/requirements.txt
--- run_dev.sh
+++ run_dev.sh
@@ -6,7 +6,7 @@ export USER_DB_BASE_URL=""
 # AWS env variables
 export S3_BUCKET_NAME=""
 
-# add in services here
+# add in services here and change the folder name and app name below to your services, using "user" service as a sample
 
 cd "$parent_path"/services/v1/user
 pip3 install --no-cache-dir -r $parent_path/services/v1/user/requirements.txt
