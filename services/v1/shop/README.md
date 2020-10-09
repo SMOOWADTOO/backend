0762d499ff2fec16cbf38adb8a6c56dc16ef14de
Chew Jia Rong <jrchew.2018@sis.smu.edu.sg>
2020-10-09 20:57:52 +0800

    Created: Shop backend service
    updated shop sql dump file to include users' usernames

---
+++ services/v1/shop/README.md
@@ -0,0 +1,210 @@
+# SHOP SERVICE
+----------
+
+The shop service handles everything related to shops: 
+
+1. Get all shops
+2. Get Shop By Shop ID
+3. Create new shop
+4. Edit shop
+
+-----------
+
+> Try to follow API best practices using nouns and not verbs (POST: /user for creating new users, GET: /user for getting user info). See more at https://docs.microsoft.com/en-us/azure/architecture/best-practices/api-design 
+
+**Port** `7002`
+
+All service calls returns the following JSON response:
+
+*Success*
+
+```
+{
+    "type": "success",
+    //whatever the function returns
+}
+```
+
+*Warning*
+
+```
+{
+    "type": "warning",
+    "message": // whatever message returned
+}
+```
+
+*Error*
+
+```
+{
+    "type": "error",
+    "message": // whatever message returned,
+    "debug": // whatever is returned as Exception (exception message)
+}
+```
+
+> **NOTE: Boolean values accept 0 as false, and non-zero for true when inserting into the database**
+
+
+# Shop
+
+## 1.1 Get All Shops
+
+**Get list of all shops**
+
+**URL** `/shops`
+
+**Method** `GET`
+
+Request sample:
+
+```
+http://localhost:7002/shops
+```
+
+This function returns the following JSON response:
+
+```
+{
+  "shops": [
+    {
+      "address": "address", 
+      "contactNo": "91234567", 
+      "createdAt": "Thu, 08 Oct 2020 22:45:31 GMT", 
+      "email": "test@email.com", 
+      "shopDesc": "Description", 
+      "shopId": 1, 
+      "shopImageURL": "hello.url.com", 
+      "shopName": "Hello", 
+      "updatedAt": "Fri, 09 Oct 2020 17:50:54 GMT", 
+      "username": "user1", 
+      "website": "abc.com"
+    }
+  ]
+}
+```
+
+## 1.2 Get Shop By Shop ID
+
+**Get shop details of a specific shop**
+
+**URL** `/shop/<shopId>`
+
+**Method** `GET`
+
+Request sample:
+
+```
+http://localhost:7002/shop/1
+```
+
+This function returns the following JSON response:
+
+```
+{
+  "shop": {
+      "address": "address", 
+      "contactNo": "91234567", 
+      "createdAt": "Thu, 08 Oct 2020 22:45:31 GMT", 
+      "email": "test@email.com", 
+      "shopDesc": "Description", 
+      "shopId": 1, 
+      "shopImageURL": "hello.url.com", 
+      "shopName": "Hello", 
+      "updatedAt": "Fri, 09 Oct 2020 17:50:54 GMT", 
+      "username": "user1", 
+      "website": "abc.com"
+    }, 
+  "type": "success"
+}
+```
+
+## 1.3 Create new shop
+
+**User create a new shop**
+
+**URL** `/shop/create`
+
+**Method** `POST`
+Request sample:
+```
+http://localhost:7002/shop/create
+```
+
+```
+{
+	"shopId": 1,
+    "productName": "coffee cake",
+    "productDesc": "cake made from kopi",
+    "unitPrice": 2.5
+}
+```
+This function returns the following JSON response:
+
+```
+{
+    "product": {
+        "productDesc": "cake made from kopi",
+        "productId": 8,
+        "productName": "coffee cake",
+        "shopId": 1,
+        "unitPrice": 2.5
+    },
+    "type": "success"
+}
+```
+
+## 1.4 Edit new shop
+
+**User can edit a shop details**
+
+**URL** `/shop/edit/<shopId>`
+
+**Method** `POST`
+Request sample:
+```
+http://localhost:7002/shop/edit
+```
+```
+{
+	"shopId": 1,
+    "username": "abc",
+    "shopName": "hello",
+    "address": "address", 
+    "contactNo": "91234567", 
+    "email": "test@email.com", 
+    "shopDesc": "Description", 
+    "shopImageURL": "hello.url.com", 
+    "shopName": "Hello", 
+    "username": "user1", 
+    "website": "abc.com"
+}
+```
+Compulsory fields: shopId, username
+
+Non-editable fields: shopId, username, createdAt, updatedAt
+
+This function returns the following JSON response:
+
+```
+{
+    "shop": {
+        "shopId": 1,
+        "username": "abc",
+        "shopName": "hello",
+        "address": "address", 
+        "contactNo": "91234567", 
+        "createdAt": "Thu, 08 Oct 2020 22:45:31 GMT", 
+        "email": "test@email.com", 
+        "shopDesc": "Description", 
+        "shopImageURL": "hello.url.com", 
+        "shopName": "Hello", 
+        "updatedAt": "Fri, 09 Oct 2020 17:50:54 GMT", 
+        "username": "user1", 
+        "website": "abc.com"
+    },
+    "type": "success"
+}
+```
+Note: shopId is auto-incremented
---
+++ services/v1/shop/shop.py
@@ -0,0 +1,216 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address, get_ipaddr
+from sqlalchemy.dialects.mysql import VARCHAR, BIGINT, TIMESTAMP, TINYINT, LONGTEXT, DATE, DATETIME, INTEGER
+import base64, time, datetime, json, uuid, os, boto3
+import traceback
+from mimetypes import guess_extension
+from urllib.request import urlretrieve, urlcleanup
+
+app = Flask(__name__)
+
+DB_BASE_URL = os.environ["SHOP_DB_BASE_URL"]
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_BASE_URL + '/shop'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_POOL_SIZE'] = 100
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 280
+ 
+db = SQLAlchemy(app)
+CORS(app)
+
+# ======================================================================
+
+# ======= AWS SETUP =======
+
+AWS_S3_CLIENT = boto3.client("s3")
+AWS_S3_RESOURCE = boto3.resource("s3")
+BUCKET_NAME = ""
+
+# ======= AWS SETUP =======
+
+# ======================================================================
+
+# ====== API SETUP ======
+
+# Rate limiter to prevent abuse and runaway Cloud usage; per IP Address
+limiter = Limiter(app, key_func=get_ipaddr, default_limits=["10 per minute", "100 per hour", "300 per day"])
+
+# Default error handling messages
+@app.errorhandler(404)
+def errorHandler(e):
+    return jsonify({"type": "error", "message": "Address not found."}), 404
+
+@app.errorhandler(405)
+def methodNotAllowedHandler(e):
+    return jsonify({"type": "error", "message": "Method not allowed."}), 405
+
+@app.errorhandler(429)
+def ratelimitHandler(e):
+    return jsonify({"type": "error", "message": "Rate limit exceeded %s" % e.description}), 429
+
+@app.errorhandler(500)
+def unexpectedExceptionHandler(e):
+    return jsonify({"type": "error", "message": "An unexpected exception occurred: %s" % e.description}), 500
+
+# ====== API SETUP ======
+
+########################################################################
+# homebiz_shop --- TABLE Shop
+########################################################################
+
+class Shop(db.Model):
+    __tablename__ = "shop"
+
+    shopId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(255), nullable=False)
+    shopName = db.Column(db.String(255), nullable=False)
+    shopDesc = db.Column(LONGTEXT, nullable=True)
+    shopImageURL = db.Column(LONGTEXT, nullable=True)
+    contactNo = db.Column(db.String(30), nullable=True)
+    address = db.Column(LONGTEXT, nullable=True)
+    email = db.Column(db.String(255), nullable=True)
+    website = db.Column(db.String(255), nullable=True)
+    createdAt = db.Column(TIMESTAMP(), nullable=False)
+    updatedAt = db.Column(TIMESTAMP(), nullable=False)
+
+    def __init__(self, username, shopName, shopDesc=None, shopImageURL=None, contactNo=None, address=None, email=None, website=None):
+        # self.shopId = shopId # no need since shopId is set with autoincrement=True
+        self.username = username
+        self.shopName = shopName
+        self.shopDesc = shopDesc
+        self.shopImageURL = shopImageURL
+        self.contactNo = contactNo
+        self.address = address
+        self.email = email
+        self.website = website
+    
+    def details(self):
+        return {
+            "shopId": self.shopId,
+            "username": self.username,
+            "shopName": self.shopName,
+            "shopDesc": self.shopDesc,
+            "shopImageURL": self.shopImageURL,
+            "contactNo": self.contactNo,
+            "address": self.address,
+            "email": self.email,
+            "website": self.website,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt,
+        }
+
+##########
+# METHODS
+##########
+
+# Get all products
+@app.route("/shop/all", methods=['GET'])
+def getAllShops():
+    try:
+        return {
+            'shops': [
+                shop.details() for shop in Shop.query.all()
+            ], 
+            'type': 'success'
+        }, 200
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {"type": "error", 
+            "message": "An error occurred when getting all shops.", 
+            "debug": str(e)}
+        ), 404
+
+# Get shop details of a specific shop
+@app.route("/shop/<string:shopId>", methods=['GET'])
+def getShopByShopId(shopId):
+    try:
+        # data = Product.query.filter_by(shopId=shopId)
+        shop = Shop.query.filter_by(shopId=shopId).first()
+        if shop:
+            return {
+                "shop": shop.details(),
+                'type': 'success'
+            }, 200
+        return {
+            "type": "error",
+            "message": "shopId " + shopId + " does not exist."
+        }, 404
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {"type": "error", 
+            "message": "An error occurred when getting all shops.", 
+            "debug": str(e)}
+        ), 500
+
+# Create new shop
+@app.route("/shop/create", methods=["POST"])
+def createShop():
+    try:
+        shop_obj = request.get_json()
+        username = shop_obj.get("username")
+        if username is None:
+            return jsonify({"type": "error", "message": "username is required"}), 500
+        shopName = shop_obj.get("shopName")
+        if shopName is None:
+            return jsonify({"type": "error", "message": "shopName is required"}), 500
+        
+        new_shop = Shop(**shop_obj)
+        db.session.add(new_shop)
+        db.session.commit()
+        return jsonify({"type": "success", "shop": new_shop.details()}), 201
+    
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        return jsonify(
+            {"type": "error", 
+            "message": "An error occurred when creating a new shop.", 
+            "debug": str(e)}
+        ), 500
+
+# Edit shop details
+@app.route("/shop/edit", methods=["POST"])
+def editShop():
+    try:
+        shop_obj = request.get_json()
+        shopId = shop_obj.get("shopId")
+        if shopId is None:
+            return jsonify({"type": "error", "message": "shopId is required"}), 500
+        shop = Shop.query.filter_by(shopId=shopId).first()
+        if shop is None:
+            return {
+                "type": "error",
+                "message": "shopId " + str(shopId) + " does not exist."
+            }, 500
+
+        username = shop_obj.get("username")
+        if username is None:
+            return jsonify({"type": "error", "message": "username is required"}), 500
+        
+        forbidden_fields = ['shopId', 'username', 'createdAt', 'updatedAt']
+        for k,v in shop_obj.items():
+            if k not in forbidden_fields:
+                setattr(shop, k, v)
+        setattr(shop, 'updatedAt', datetime.datetime.now())
+
+        db.session.commit()
+        return jsonify({"type": "success", "shop": shop.details()}), 201
+    
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        return jsonify(
+            {"type": "error", 
+            "message": "An error occurred when editing shop",
+            "debug": str(e)}
+        ), 500
+
+# ======================================================================
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7002, debug=True)
